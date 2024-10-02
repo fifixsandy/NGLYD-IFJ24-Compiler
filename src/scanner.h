7063a26bb20d6068f2cbd191db59cbd50f87585f
Tibor <tibormalega3@gmail.com>
2024-10-02 13:40:47 +0200

    Added unfinished header file for scanner

---
+++ src/scanner.h
@@ -0,0 +1,50 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+typedef enum {
+    tokentype_EOL,
+    tokentype_EOF,
+
+    tokentype_vbar,
+    tokentype_semicolon,
+    tokentype_notequal,
+
+    tokentype_lbracket,
+    tokentype_rbracket,
+
+    tokentype_plus,
+    tokentype_minus,
+    tokentype_divide,
+    tokentype_comment,
+    tokentype_multiply,
+    tokentype_assign,
+    tokentype_equal,
+    tokentype_lower,
+    tokentype_lowerequal,
+    tokentype_greater,
+    tokentype_greaterequal,
+    tokentype_import,
+    tokentype_dot,
+    tokentype_colon,
+    tokentype_string,
+    tokentype_id,
+    tokentype_pseudovar,
+    tokentype_zeroint,
+    tokentype_int,
+    tokentype_float,
+    tokentype_exponentialnum,
+
+}token_types;
+
+typedef struct {
+    token_types type;
+    int line;
+    int column;
+    char* value;
+} Token;
+
