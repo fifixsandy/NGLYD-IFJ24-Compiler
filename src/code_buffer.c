f522a62822e7d58e7364fa6cdb43dbe4bae78142
Ján Skovajsa <jan.skovajsa33@gmail.com>
2024-11-10 23:26:16 +0100

    New implemetation for code_buffer, for better optimaliztion according to assigment

--- src/code_buffer.c
+++ src/code_buffer.c
@@ -8,78 +8,102 @@
  */
 #include "code_buffer.h"
 
-#define INIT_SIZE_OF_BUFFER 11
-
-bool buf_init(Buffer **buf){
-    *buf = malloc(sizeof(Buffer));
+bool buf_init(Buffer_ll **buf){
+    *buf = malloc(sizeof(Buffer_ll));
     if (*buf == NULL) {
         return false;
     }
-    (*buf)->str = malloc(sizeof(char)*INIT_SIZE_OF_BUFFER);
-    if((*buf)->str == NULL){
-        return false;
-    }
-    (*buf)->str[0] = '\0';
-    (*buf)->length = 0;
-    (*buf)->alloc_size = INIT_SIZE_OF_BUFFER-1;
+    (*buf)->first = NULL;
+    (*buf)->last = NULL;
+    (*buf)-> flag = NULL;
+    (*buf)->tmp = NULL;
     return true;
 }
 
-bool buf_expand(Buffer *buf, size_t min_add_size){
-    size_t next_size = buf->alloc_size * 2 + min_add_size;
-    char *tmp = realloc(buf->str, next_size * sizeof(char));
+Buffer_node *buf_new_node(){
+    Buffer_node *new = malloc(sizeof(Buffer_node));
+    if(new == NULL){
+        return NULL;
+    }
+    new->next = NULL;
+    new->str = NULL;
+    return new;
+}
+
+bool buf_add(Buffer_ll *buf, char *str){
+    char *tmp = realloc(buf->tmp, sizeof(char)*(strlen(str)+1));
     if(tmp == NULL){
         return false;
     }
-    buf->str = tmp;
-    buf->alloc_size = next_size;
-    return true;
-}
-
-bool buf_push_code(Buffer *buf, char *str){
-    size_t len_str = strlen(str);
-    if(buf->length + len_str > buf->alloc_size){
-        if(!buf_expand(buf, len_str)) return false;
+    if(buf->tmp == NULL){
+        buf->tmp = tmp;
+        strcpy(buf->tmp, str);
     }
-    strcat(buf->str, str);
-    buf->length += len_str;
-    return true;
-}
-
-bool buf_push_int(Buffer *buf, int num){
-    char str[30];  
-    sprintf(str, "%d", num);
-    if(!buf_push_code(buf, str)) return false;
-    return true;
-}
-
-bool buf_push_float(Buffer *buf, float num){
-    char str[100];  
-    sprintf(str, "%a", num);
-    if(!buf_push_code(buf, str)) return false;
-    return true;
-}
-
-bool buf_push_string(Buffer *buf, char *str){
-    char tmp[5];
-    for(size_t i = 0; i < strlen(str); i++){
-        if(str[i] <= 32 || str[i] == 35 || str[i] == 92){
-            sprintf(tmp, "\\%03d", str[i]);
-            if(!buf_push_code(buf, tmp)) return false;
-        }
-        else{
-            sprintf(tmp, "%c", str[i]);
-            if(!buf_push_code(buf, tmp)) return false;
-        }
+    else{
+        buf->tmp = tmp;
+        strcat(buf->tmp, str);
     }
-
     return true;
 }
 
-bool fprint_buffer(Buffer *buf, FILE *stream) {
-    if (buf == NULL || buf->str == NULL || stream == NULL) {
+bool buf_push_code(Buffer_ll *buf){
+    Buffer_node *new = buf_new_node();
+    if(new == NULL){
         return false;
     }
-    fprintf(stream, "%s", buf->str);
+    if(buf->first == NULL){
+        buf->first = new;
+    }
+    else{
+        buf->last->next = new;
+    }
+    buf->last = new;
+    new->str = buf->tmp;
+    buf->tmp = NULL;
+    return true;
+}
+
+bool buf_add_push(Buffer_ll *buf, char *str){
+    if(!buf_add(buf, str)) return false;
+    if(!buf_push_code(buf)) return false;
+    return true;
+}
+
+
+void buf_add_flag(Buffer_ll *buf){
+    buf->flag = buf->last;
+}
+
+void buf_delete_flag(Buffer_ll *buf){
+    buf->flag = NULL;
+}
+
+bool buf_push_after_flag(Buffer_ll *buf){
+    if(buf->flag == NULL) return false;
+    Buffer_node *new = buf_new_node();
+    if(new == NULL){
+        return false;
+    }
+    
+    new->str = buf->tmp;
+    buf->tmp = NULL;
+    
+    if(buf->flag == buf->last){
+        buf->last = new;
+    }
+    new->next = buf->flag->next;
+    buf->flag->next = new;
+    return true;
+}
+
+bool fprint_buffer(Buffer_ll *buf, FILE *stream) {
+    if (buf == NULL || stream == NULL) {
+        return false;
+    }
+    Buffer_node *tmp = buf->first;
+    while(tmp != NULL){
+        fprintf(stream, "%s", tmp->str);
+        tmp = tmp->next;
+    }
     return true;
 }
