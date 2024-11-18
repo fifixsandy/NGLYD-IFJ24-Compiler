d959160c82211c3458dd7348217f2e85e9ce2ae4
dotZupan <mati.figi@gmail.com>
2024-11-18 01:51:05 +0100

    added semantic check for division by zero

--- src/expression_stack.c
+++ src/expression_stack.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "expression_stack.h"
 
+#define EPSILON 1e-6    // 0.000001
+
 precedence precedence_table[14][14] = {
 //    *   /   +   -   ==  !=  <   >   <=  >=  (   )   i   $
     { GR, GR, GR, GR, GR, GR, GR, GR, GR, GR, LS, GR, LS, GR},  // *    
@@ -488,6 +490,22 @@ void semantic_check_retype(stack_item *left_operand, stack_item *operator, stack
     }
     if((left_operand->control->is_nullable == true || right_operand->control->is_nullable == true) && operator->expr != NOT_EQUAL && operator->expr != EQUAL){
         ERROR(ERR_SEM_TYPE, "Operand with null cannot be used in expression\n");
+    }
+
+    if(right_operand->control->known_during_compile == true && operator->expr == DIVISION){
+        dataType typeZ = right_operand->node->nodeRep.literalNode.dataT;
+        if( typeZ == f64){
+            float fvalue = right_operand->node->nodeRep.literalNode.value.floatData;
+            if( fabs(fvalue) < EPSILON){        // if expresion is devided by zero or very small number
+                ERROR(ERR_SEM_TYPE, "Expression can't be devided by zero\n");
+            }
+        }
+        if(typeZ == i32){
+            int ivalue = right_operand->node->nodeRep.literalNode.value.intData;
+            if(ivalue == 0){        // if expresion is devided by zero
+                ERROR(ERR_SEM_TYPE, "Expression can't be devided by zero\n");
+            }
+        }
     }
 
     control->known_during_compile = false;
